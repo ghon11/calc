a9c8e2cf59643c7805f07777d251666b1486ed78
Garry Yang <yarrygang@gmail.com>
2022-09-11 20:45:21 +0100

    Add calc functions & implement basic html/css.
    Basic calculator math functions and operate function. Bones of a
    calculator structure started in html/css.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+add = function(a, b) {
+    return a + b;
+};
+
+subtract = function(a, b) {
+    return a - b;
+};
+
+multiply = function(a, b) {
+    return a * b;
+};
+
+divide = function(a, b,) {
+    return a / b;
+};
+
+operate = function(a, b, operator) {
+    return operator(a, b);
+}
+
+console.log(`5 + 4 = ${add(5,4)}`);
+console.log(`5 - 4 = ${subtract(5,4)}`);
+console.log(`5 * 4 = ${multiply(5,4)}`);
+console.log(`10 / 2 = ${divide(10,2)}`);
+
+console.log(operate(5,4, add));
+console.log(operate(5,4, subtract));
+console.log(operate(5,4, multiply));
+console.log(operate(10,2, divide));
