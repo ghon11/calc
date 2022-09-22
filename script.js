7f8e75f49e99791d67c5738b12fae7c37f55603d
ghon11 <yarrygang@gmail.com>
2022-09-22 21:14:37 +0100

    Add code for number buttons & display value
    Added: Event listener for each number button, writes number to string
    in displayValue variable. Function to update on screen display, called
    when number buttons are pressed.

--- script.js
+++ script.js
@@ -18,12 +18,69 @@ operate = function(a, b, operator) {
     return operator(a, b);
 }
 
-console.log(`5 + 4 = ${add(5,4)}`);
-console.log(`5 - 4 = ${subtract(5,4)}`);
-console.log(`5 * 4 = ${multiply(5,4)}`);
-console.log(`10 / 2 = ${divide(10,2)}`);
+let displayValue = "";
 
-console.log(operate(5,4, add));
-console.log(operate(5,4, subtract));
-console.log(operate(5,4, multiply));
-console.log(operate(10,2, divide));
+const oneBtn = document.querySelector("#one");
+oneBtn.addEventListener('click', () => {
+  displayValue += 1;
+  updateValue();
+});
+
+const twoBtn = document.querySelector("#two");
+twoBtn.addEventListener('click', () => {
+  displayValue += 2;
+  updateValue();
+});
+
+const threeBtn = document.querySelector("#three");
+threeBtn.addEventListener('click', () => {
+  displayValue += 3;
+  updateValue();
+});
+
+const fourBtn = document.querySelector("#four");
+fourBtn.addEventListener('click', () => {
+  displayValue += 4;
+  updateValue();
+});
+
+const fiveBtn = document.querySelector("#five");
+fiveBtn.addEventListener('click', () => {
+  displayValue += 5;
+  updateValue();
+});
+
+const sixBtn = document.querySelector("#six");
+sixBtn.addEventListener('click', () => {
+  displayValue += 6;
+  updateValue();
+});
+
+const sevenBtn = document.querySelector("#seven");
+sevenBtn.addEventListener('click', () => {
+  displayValue += 7;
+  updateValue();
+});
+
+const eightBtn = document.querySelector("#eight");
+eightBtn.addEventListener('click', () => {
+  displayValue += 8;
+  updateValue();
+});
+
+const nineBtn = document.querySelector("#nine");
+nineBtn.addEventListener('click', () => {
+  displayValue += 9;
+  updateValue();
+});
+
+const zeroBtn = document.querySelector("#zero");
+zeroBtn.addEventListener('click', () => {
+  displayValue += 0;
+  updateValue();
+});
+
+function updateValue() {
+    document.getElementsByClassName("display")[0].innerText = displayValue;
+    console.log(displayValue);
+}
